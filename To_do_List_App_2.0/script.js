f9c42cf5c7f72cdc409a6af91489b76c177af2c8
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-04-21 23:03:01 +0300

    Started the TODOLISTAPP

---
+++ To_do_List_App_2.0/script.js
@@ -0,0 +1,8 @@
+( () => {
+    let toDoListArray = [];
+
+    const form = document.querySelector(".form");
+    const input = form.querySelector(".form_input");
+    const ul= document.querySelector(".toDoList")
+    
+})
