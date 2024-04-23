f86841be185d1d56c78960554fc8419d13cfe9fd
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-04-23 12:53:14 +0300

    Finished Building Task Management app

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # To do list App
+# Task_Management App
+
+# Firebase SDK Below
+    <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCwHj2zOHiB3vVDOzjTzV-UDzzWihzhBfg",
+    authDomain: "plp-projects-d957d.firebaseapp.com",
+    projectId: "plp-projects-d957d",
+    storageBucket: "plp-projects-d957d.appspot.com",
+    messagingSenderId: "922020316957",
+    appId: "1:922020316957:web:6898c2128a3d3e216c83f4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+    </script>
---
+++ Task_Management_App/app.js
@@ -0,0 +1,54 @@
+// initialize Firebase with your config
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCwHj2zOHiB3vVDOzjTzV-UDzzWihzhBfg",
+    authDomain: "plp-projects-d957d.firebaseapp.com",
+    projectId: "plp-projects-d957d",
+});
+
+const db = firebase.firestore();
+
+// Function to add a task
+function addTask(){
+    const taskInput = document.getElementById("task-input");
+    const task = task.Input.value.trim();
+
+    if (task !== ""){
+        db.collection("tasks").add({
+            task: task,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        taskInput.value = ""
+    }
+}
+
+// function to render tasks
+function rendertasks(doc){
+    const taskList = document.getElementById("task-list");
+    const taskItem = document.createElement("li");
+    taskItem.className = "task-item";
+    taskItem.innerHTML = `
+    <span>${doc.data().task}</span>
+    <button onclick = "deleteTask('${doc.id}')">Delete</button>
+    `;
+    taskList.appendChild(taskItem);
+}
+
+// Real-time listener for tasks
+
+db.collection("tasks")
+    .orderBy("timestamp", "desc")
+    .onSnapshot(snapshot => {
+        const changes = snapshot.docChanges();
+        changes.forEach(change => {
+            if (change.type === "added"){
+                rendertasks(change.doc);
+            }
+        });
+    });
+
+// function to delete task
+function deleteTask (id) {
+    db.collection("tasks").doc(id).delete();
+}
+
